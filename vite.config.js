36409506e537d8e0058fc6dc3c59bccdace445df
Avinash_Nalawade <harshnalawade244@gmail.com>
2025-09-22 20:42:52 +0530

    /

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/my-portfolio/',  // 👈 repo name here
+    base: '/',  // 👈 repo name here
 })
